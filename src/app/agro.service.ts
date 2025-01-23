b76bc9c11a0f39a919b5bfe11fdf0bc188f8b221
Betsya <betsq9@gmail.com>
2025-01-23 13:59:58 +0200

    s

--- src/app/agro.service.ts
+++ src/app/agro.service.ts
@@ -21,7 +21,7 @@ export class AgroServiceService {
     this.profileData = data;
   }
   getProfileData(user_id: any) {
-    return this.http.get(`http://localhost:8077/api/user/$`);
+    return this.http.get(`http://localhost:8077/api/user/${user_id}`);
   }
 
   // Метод для POST-запиту
